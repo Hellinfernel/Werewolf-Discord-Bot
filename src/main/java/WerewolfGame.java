d59e47dfef484d7155d4f93b6297aa6ce8988522
Hellinfernel <55323575+Hellinfernel@users.noreply.github.com>
2022-01-13 15:01:26 +0100

    - changed the way to coordinate the game to gameContinue to be more reactive.

--- src/main/java/WerewolfGame.java
+++ src/main/java/WerewolfGame.java
@@ -15,6 +15,7 @@ public class WerewolfGame {
 
 
 
+
     ArrayList<WerewolfGameRole> playerList;
    // ArrayList<WerewolfEvent> DayEvent;
     // ArrayList<WerewolfEvent> NightEvent;
@@ -39,7 +40,7 @@ public class WerewolfGame {
 
                                 })); */
 
-                gameRun();
+                gameContinue();
 
 
 
@@ -70,7 +71,7 @@ public class WerewolfGame {
         MessageCreateSpec spec = MessageCreateSpec.create();
         stringBuilder.append("Good Morning!");
 
-        mainChannel.createMessage();
+        mainChannel.createMessage().withContent("").subscribe();
 
 
     }
@@ -86,93 +87,91 @@ public class WerewolfGame {
 
 
 
-        werewolfChannel.createMessage(MessageCreateSpec.create().nonceOrElse("Good Night, my fellow Werewolfes! Which one do we want to hunt?"));
+        werewolfChannel.createMessage(MessageCreateSpec.create().nonceOrElse("Good Night, my fellow Werewolfes! Which one do we want to hunt?")).subscribe();
         Main.commands.put("kill", event -> {
 
-            if( event.getMessage()
-                    .getChannel()
-                    .equals(werewolfChannel)) { //fragt ab, ob der spieler in den richtigen channel schreibt
-
-                Member votingPlayer = event.getMember().get();
-
-                if (votingWerewolfes.get(votingPlayer) == false) { //Fragt ab, ob der Spieler bereits abgestimmt hat :D
+            Mono <Void> run = internWerewolfEvent(votingWerewolfes, votedVictims, event);
 
 
-                    Mono<Member> targetPlayer = event.getMessage()
-                            .getMemberMentions()
-                            .get(0)
-                            .asFullMember();
-                      if (/*targetPlayer.hasElement(). == true*/true){ //yeah i know that doesnt work :,D
-
-                        alivePlayers().stream()
-                                .filter(y -> y.getClass()
-                                        .isAssignableFrom(Villager.class))
-                                .filter(z -> z.player.equals(targetPlayer))
-                                .findFirst()
-                                .ifPresent(victim -> {
-                                    votingWerewolfes.replace(votingPlayer,false,true);
-                                    if (votedVictims.containsKey(victim.player)){
-                                        int voteCounter = votedVictims.get(victim) + 1;
-                                        votedVictims.replace(victim, voteCounter);
-                                        werewolfChannel.createMessage().withContent(victim.player.getTag()+ " got a vote. He/She/It has now "+ voteCounter + " votes.");
-
-                                    }
-                                    else{
-                                        votedVictims.put(victim,1);
-                                        werewolfChannel.createMessage().withContent(victim.player.getTag() + " got a vote. He/She/It has now 1 vote.");
-                                    }
-
-                                    long remainingWerewolfVotes = votingWerewolfes.entrySet().stream().filter(x -> x.getValue() == false).count();
-                                    long highestVoting = votedVictims.values().stream().mapToInt(x -> x).filter(x -> x >= 0).max().orElse(0); //finds the highest value.
-                                    long secondHighestVoting = votedVictims.values().stream().mapToInt(x -> x).filter(x -> x < highestVoting).filter(x -> x >= 0).max().orElse(0); //finds the second highest value.
-                                    if (highestVoting - secondHighestVoting > remainingWerewolfVotes && votedVictims.values().stream().filter(x -> x == highestVoting).count() == 1){ //used to find out if the remaining votes would be able to change the result. if not, there will be the kill.
-                                        WerewolfGameRole finalVictim = votedVictims.entrySet().stream().filter(x -> x.getValue() == highestVoting).findFirst().get().getKey();
-                                        finalVictim.isAlive = false;
-                                        werewolfChannel.createMessage().withContent(finalVictim.player.getTag() + " is the Victim :D");
-                                        Main.commands.remove("kill");
-                                    }
-                                    else if (remainingWerewolfVotes == 0){ //finds out if there are no remaining votes. this should be only reached if there is more then one with the most votes.
-                                        WerewolfGameRole finalVictim = votedVictims.entrySet().stream().filter(x -> x.getValue() == highestVoting).findAny().get().getKey();
-                                        finalVictim.isAlive = false;
-                                        werewolfChannel.createMessage().withContent("Well, seems like there is a draw. We took a random of the mostvoted guys and kill him... " + finalVictim.player.getTag() + " is our Victim.");
-                                        Main.commands.remove("kill");
-                                    }
-                                    else { //forgot what here should happen :,D
-                                        werewolfChannel.createMessage().withContent("forgot what here should happen lol");
-
-
-                                    }
+            return run;
 
 
 
 
-                                });
+        });
+
+    }
+
+    private void internWerewolfEvent(HashMap<Member, Boolean> votingWerewolfes, HashMap<WerewolfGameRole, Integer> votedVictims, discord4j.core.event.domain.message.MessageCreateEvent event) {
+        if (event.getMessage()
+                .getChannel()
+                .equals(werewolfChannel)) { //fragt ab, ob der spieler in den richtigen channel schreibt
+
+            Member votingPlayer = event.getMember().get();
+
+            if (votingWerewolfes.get(votingPlayer) == false) { //Fragt ab, ob der Spieler bereits abgestimmt hat :D
 
 
+                Mono<Member> targetPlayer = event.getMessage()
+                        .getMemberMentions()
+                        .get(0)
+                        .asFullMember();
+                if (/*targetPlayer.hasElement(). == true*/true) { //yeah i know that doesnt work :,D
+
+                    alivePlayers().stream()
+                            .filter(y -> y.getClass()
+                                    .isAssignableFrom(Villager.class))
+                            .filter(z -> z.player.equals(targetPlayer))
+                            .findFirst()
+                            .ifPresent(victim -> {
+                                votingWerewolfes.replace(votingPlayer, false, true);
+                                if (votedVictims.containsKey(victim.player)) {
+                                    int voteCounter = votedVictims.get(victim) + 1;
+                                    votedVictims.replace(victim, voteCounter);
+                                    werewolfChannel.createMessage().withContent(victim.player.getTag() + " got a vote. He/She/It has now " + voteCounter + " votes.").subscribe();
+
+                                } else {
+                                    votedVictims.put(victim, 1);
+                                    werewolfChannel.createMessage().withContent(victim.player.getTag() + " got a vote. He/She/It has now 1 vote.").subscribe();
+                                }
+
+                                long remainingWerewolfVotes = votingWerewolfes.entrySet().stream().filter(x -> x.getValue() == false).count();
+                                long highestVoting = votedVictims.values().stream().mapToInt(x -> x).filter(x -> x >= 0).max().orElse(0); //finds the highest value.
+                                long secondHighestVoting = votedVictims.values().stream().mapToInt(x -> x).filter(x -> x < highestVoting).filter(x -> x >= 0).max().orElse(0); //finds the second highest value.
+                                if (highestVoting - secondHighestVoting > remainingWerewolfVotes && votedVictims.values().stream().filter(x -> x == highestVoting).count() == 1) { //used to find out if the remaining votes would be able to change the result. if not, there will be the kill.
+                                    WerewolfGameRole finalVictim = votedVictims.entrySet().stream().filter(x -> x.getValue() == highestVoting).findFirst().get().getKey();
+                                    finalVictim.isAlive = false;
+                                    werewolfChannel.createMessage().withContent(finalVictim.player.getTag() + " is the Victim :D").subscribe();
+                                    Main.commands.remove("kill");
+                                    gameContinue();
+                                } else if (remainingWerewolfVotes == 0) { //finds out if there are no remaining votes. this should be only reached if there is more then one with the most votes.
+                                    WerewolfGameRole finalVictim = votedVictims.entrySet().stream().filter(x -> x.getValue() == highestVoting).findAny().get().getKey();
+                                    finalVictim.isAlive = false;
+                                    werewolfChannel.createMessage().withContent("Well, seems like there is a draw. We took a random of the mostvoted guys and kill him... " + finalVictim.player.getTag() + " is our Victim.").subscribe();
+                                    Main.commands.remove("kill");
+                                    gameContinue();
+                                } else { //forgot what here should happen :,D
+                                    werewolfChannel.createMessage().withContent("forgot what here should happen lol").subscribe();
 
 
+                                }
 
 
+                            });
 
-                      }
-                           else {
-                             werewolfChannel.createMessage().withContent("No valid target. the syntax is: !kill @HereThenameoftheplayer#9367");
 
-                           }
+                } else {
+                    werewolfChannel.createMessage().withContent("No valid target. the syntax is: !kill @HereThenameoftheplayer#9367").subscribe();
 
                 }
-                else {
-                    werewolfChannel.createMessage().withContent("You have already voted ;D");
-                }
 
-
+            } else {
+                werewolfChannel.createMessage().withContent("You have already voted ;D").subscribe();
             }
 
 
 
-            return null;
-        });
-
+        }
     }
 
 
@@ -182,6 +181,26 @@ public class WerewolfGame {
                 .filter(player -> player.isAlive)
                 .forEach(x -> alivePlayers.add(x));
         return alivePlayers;
+    }
+
+    void gameContinue(){ //method who should be called if the game should continue.
+        if (someoneWhoWon() == null){
+                 if (isDay==true) {
+            isDay = false;
+            werewolfEvent();
+                 }
+
+                 else {
+            isDay = true;
+            villageEvent();
+                 }
+        }
+        else {
+            someoneWhoWon().winningSequence();
+            gameEnd = true;
+        }
+
+
     }
 
 
